c0455258c1ab8ce44e1135dc8faf78bf8650be75
blvckcvt7 <134386774+blvckcvt7@users.noreply.github.com>
2024-06-05 08:24:10 +0900

    Implemented new technique to generate divs
    - Create function to develop 16 square div with loop
    - Create containers to flex the divs and create grid

--- main.js
+++ main.js
@@ -1,13 +1,33 @@
 'use strict';
 
 const container = document.querySelector('.container');
+const contTwo = document.createElement('div');
+const contThree = document.createElement('div');
+
+contTwo.className = 'cont-two';
+contThree.className = 'cont-three';
+
+container.appendChild(contTwo);
+container.append(contThree);
 
 function createDiv() {
-  for (let i = 0; i < 16; i++) {
+  for (let i = 1; i <= 16; i++) {
     let newDiv = document.createElement('div');
 
     newDiv.className = 'square';
-    container.appendChild(newDiv);
+    newDiv.textContent = i;
+    contTwo.appendChild(newDiv);
   }
 }
 createDiv();
+
+function createDiv2() {
+  for (let i = 1; i <= 16; i++) {
+    let newDiv = document.createElement('div');
+
+    newDiv.className = 'square';
+    newDiv.textContent = i;
+    contThree.appendChild(newDiv);
+  }
+}
+createDiv2();
