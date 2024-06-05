e8eca11524e8904370a03031cf37ac377256ba40
blvckcvt7 <134386774+blvckcvt7@users.noreply.github.com>
2024-06-05 20:56:07 +0900

    Added loop and removed functions
    - Create for loop to generate secondary containers
    - Create hover effect listening to mouseover event

--- main.js
+++ main.js
@@ -1,33 +1,55 @@
 'use strict';
 
 const container = document.querySelector('.container');
-const contTwo = document.createElement('div');
-const contThree = document.createElement('div');
+// const secondaryCont = document.createElement('div');
+// const terciaryCont = document.createElement('div');
 
-contTwo.className = 'cont-two';
-contThree.className = 'cont-three';
+// secondaryCont.className = 'cont-two';
+// terciaryCont.className = 'cont-three';
 
-container.appendChild(contTwo);
-container.append(contThree);
+// container.appendChild(secondaryCont);
+// container.append(terciaryCont);
 
-function createDiv() {
-  for (let i = 1; i <= 16; i++) {
+for (let i = 1; i <= 16; i++) {
+  let secondaryCont = document.createElement('div');
+
+  secondaryCont.className = 'secondary-container';
+  container.appendChild(secondaryCont);
+
+  for (let j = 1; j <= 16; j++) {
     let newDiv = document.createElement('div');
 
     newDiv.className = 'square';
-    newDiv.textContent = i;
-    contTwo.appendChild(newDiv);
+    newDiv.textContent = j;
+    secondaryCont.appendChild(newDiv);
   }
 }
-createDiv();
 
-function createDiv2() {
-  for (let i = 1; i <= 16; i++) {
-    let newDiv = document.createElement('div');
+const squares = document.querySelectorAll('.square'); //it creates a nodelist
+squares.forEach((square) => {
+  square.addEventListener('mouseover', () => {
+    square.classList.add('hovered');
+  });
+});
 
-    newDiv.className = 'square';
-    newDiv.textContent = i;
-    contThree.appendChild(newDiv);
-  }
-}
-createDiv2();
+// function createDiv() {
+//   for (let i = 1; i <= 16; i++) {
+//     let newDiv = document.createElement('div');
+
+//     newDiv.className = 'square';
+//     newDiv.textContent = i;
+//     contTwo.appendChild(newDiv);
+//   }
+// }
+// createDiv();
+
+// function createDiv2() {
+//   for (let i = 1; i <= 16; i++) {
+//     let newDiv = document.createElement('div');
+
+//     newDiv.className = 'square';
+//     newDiv.textContent = i;
+//     contThree.appendChild(newDiv);
+//   }
+// }
+// createDiv2();
